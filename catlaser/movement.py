ff6217c3877e28ef2454f2e18c88ea319d04d719
Peter Boone <boonepeterg@gmail.com>
2019-09-02 15:49:33 +0100

    nothing

--- catlaser/movement.py
+++ catlaser/movement.py
@@ -1,13 +1,17 @@
 import init_laser as laser
 
-XPins = [3, 4, 17, 27]
-YPins = [22, 10, 21, 20]
+XPins = [12, 25, 24, 23]
+YPins = [4, 17, 27, 22]
+Laser_Pin = 16
+
+testlaser = laser.Laser(XPins, YPins, Laser_Pin, 0.005)
+
+testlaser.Laser_On()
 
 
-testlaser = laser.Laser(XPins, YPins, 0.005)
-
-with open("test2.csv", "r") as smallcsv:
+with open("test.csv", "r") as smallcsv:
     for line in smallcsv:
         xy = line.strip().split(',')
         testlaser.MoveAbsolute(int(xy[0]), int(xy[1]))
+testlaser.Laser_Off()
 testlaser.MoveAbsolute(0,0)
