9c8eed4a4eb62beebb73e264ad61ee4407696727
Peter Boone <boonepeterg@gmail.com>
2019-10-19 16:31:16 -0400

    update movement

--- catlaser/movement.py
+++ catlaser/movement.py
@@ -7,15 +7,14 @@ Laser_Pin = 16
 testlaser = laser.Laser(XPins, YPins, Laser_Pin, 0.005)
 
 testlaser.Laser_On()
+testlaser.Laser_Off()
 
 
 
 
-"""
 with open("test.csv", "r") as smallcsv:
     for line in smallcsv:
         xy = line.strip().split(',')
         testlaser.MoveAbsolute(int(xy[0]), int(xy[1]))
 testlaser.Laser_Off()
 testlaser.MoveAbsolute(0,0)
-"""
