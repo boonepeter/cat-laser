7bff3bb65aef8d9ef9148a08e686ce39e936ac5f
Peter Boone <boonepeterg@gmail.com>
2019-10-19 17:23:02 -0400

    readint and print

--- catlaser/controller.py
+++ catlaser/controller.py
@@ -4,5 +4,29 @@ gamepad = InputDevice('/dev/input/event0')
 print(gamepad)
 
 for event in gamepad.read_loop():
-    print(event)
-    print(categorize(event))
+    if event.type == ecodes.EV_KEY:
+        if event.code == 296:
+            print("select")
+        elif event.code == 297:
+            print("start")
+        elif event.code == 291:
+            print("Y")
+        elif event.code == 288:
+            print("X")
+        elif event.code == 290:
+            print("B")
+        elif event.code == 289:
+            print("A")
+        elif event.code == 293:
+            print("R Trig")
+        elif event.code == 292:
+            print("L Trig")
+        else:
+            print("unknown")
+    elif event.type == ecodes.EV_ABS:
+        if event.code == 0:
+            print("X")
+        elif event.code == 1:
+            print("Y")
+        else:
+            print("Unknown")
