d91c61e2b26787f117d040adcb70833f3cb23c7a
Peter Boone <boonepeterg@gmail.com>
2019-10-19 21:47:59 +0100

    typo

--- catlaser/controller.py
+++ catlaser/controller.py
@@ -3,5 +3,5 @@ from evdev import InputDevice, categorize, ecodes
 gamepad = InputDevice('/dev/input/event0')
 print(gamepad)
 
-for event in gamepad.read_loop()
+for event in gamepad.read_loop():
     print(categorize(event))
