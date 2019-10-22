d1af34825bf48aae3e2a84ef8e9f154c38293000
Peter Boone <boonepeterg@gmail.com>
2019-10-21 20:17:30 -0400

    tutorial was wrong

--- catlaser/servo.py
+++ catlaser/servo.py
@@ -6,8 +6,8 @@ pin_one = 17
 pin_two = 27
 GPIO.setup(pin_one, GPIO.OUT)
 tilt_y = GPIO.PWM(pin_one, 50)
-tilt_y = start(0)
+tilt_y.start(0)
 tilt_y.ChangeDutyCycle(2)
 tilt_y.ChangeDutyCycle(5)
-tilt_y = stop()
+tilt_y.stop()
 GPIO.cleanup()
