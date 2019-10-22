30f122fb4da94b613a8e3656898236b6d7d6e1c4
Peter Boone <boonepeterg@gmail.com>
2019-10-21 20:16:33 -0400

    typo

--- catlaser/servo.py
+++ catlaser/servo.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 pin_one = 17
 pin_two = 27
 GPIO.setup(pin_one, GPIO.OUT)
-tilt_y = GPIO.PWN(pin_one, 50)
+tilt_y = GPIO.PWM(pin_one, 50)
 tilt_y = start(0)
 tilt_y.ChangeDutyCycle(2)
 tilt_y.ChangeDutyCycle(5)
