860d0714c7780775619d766711e559c75ffe42dc
Peter Boone <boonepeterg@gmail.com>
2019-11-23 14:45:01 -0500

    servo

--- catlaser/servo.py
+++ catlaser/servo.py
@@ -1,3 +1,26 @@
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)
+y_servo = 19
+x_servo = 18
+
+class Motors():
+    def __init__(self, xpin, ypin):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(xpin, GPIO.OUT)
+        GPIO.setup(ypin, GPIO.OUT)
+        self.x = GPIO.PWM(xpin, 50)
+        self.y = GPIO.PWM(ypin, 50)
+        self.x.start(0)
+        self.y.start(0)
+    def Move(self, percent):
+        self.x.ChangeDutyCycle(percent)
+        self.y.ChangeDutyCycle(percent)
+
+
+motor = Motors(x_servo, y_servo)
+for i in range(10):
+    motor.Move(i * 10)
+    time.sleep(.1)
+    motor.Move(0)
+    time.sleep(1)
