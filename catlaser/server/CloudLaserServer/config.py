3bb5c436a47b151312074adf3eb093ddbf4bbe68
Ubuntu <ubuntu@ip-172-26-8-97.ec2.internal>
2019-12-02 05:53:18 +0000

    config changes on machine

--- catlaser/server/CloudLaserServer/config.py
+++ catlaser/server/CloudLaserServer/config.py
@@ -4,13 +4,13 @@
 
 # Secret key should be set to a random value (like you're making a new password).
 # You don't need to remember this value, instead it's used to secure session state.
-SECRET_KEY = 'some random secret value!'
+SECRET_KEY = 'beth has two cats'
 
 # Set MJPEG_URL to the URL of the mjpeg-proxy stream that displays video:
-MJPEG_URL = 'http://<your cloud server domain/DNS name>:8080/index1.jpg'
+MJPEG_URL = 'http://50.16.5.232:8080/index1.jpg'
 
 # Amount of time (in seconds) each active player has to play:
-PLAYTIME_SECONDS = 60.0
+PLAYTIME_SECONDS = 120.0
 
 # Hostname for the MQTT server that the Pi will listen to for commands.
 MQTT_HOST = 'localhost'
